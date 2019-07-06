61ba4db3c65a4a7d394aa5bb2f5927a43119c0ed
Miguel Simon Kreuz <s.carton@gmail.com>
2019-07-06 10:43:40 +0200

    first pass at concourse CI

---
+++ concourse-ci/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+apt-get update && apt install -y curl build-essential
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y git-lfs
+git lfs install
+
+source manage.sh work_in_python_version_no_tests ${IC_PYTHON_VERSION}
+
+HYPOTHESIS_PROFILE=hard bash manage.sh run_tests_par 4
