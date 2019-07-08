69411cfbb1c37bac51a3fb1c534d4a1470e93712
Miguel Simon Kreuz <s.carton@gmail.com>
2019-07-08 18:43:05 +0200

    github-pr-resource doesn't pull git lfs by default, try to pull manually

--- concourse-ci/run-tests.sh
+++ concourse-ci/run-tests.sh
@@ -6,8 +6,11 @@ apt-get update && apt install -y curl build-essential
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install -y git-lfs
+
 git lfs install
+git lfs pull
 
 source manage.sh work_in_python_version_no_tests ${IC_PYTHON_VERSION}
+
 
 HYPOTHESIS_PROFILE=hard bash manage.sh run_tests_par 2
