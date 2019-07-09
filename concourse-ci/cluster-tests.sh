8a701dd0deb93a95701645e4800beb8a2ed6809a
Miguel Simon Kreuz <s.carton@gmail.com>
2019-07-09 18:48:59 +0200

    more realistic pipeline

--- concourse-ci/cluster-tests.sh
+++ concourse-ci/cluster-tests.sh
@@ -5,6 +5,37 @@ set -e
 # error out if the credentials haven't been provided
 : "${FAKE_CLUSTER_SSH_PRIVATE_KEY:?}"
 
+get_output () {
+  "${ICTDIR:?}"
+  "${OUTDIR:?}"
+
+  cd $ICTDIR
+  source manage.sh work_in_python_version_no_tests ${IC_PYTHON_VERSION}
+
+  city irene \
+    -i $ICDIR/database/test_data/electrons_40keV_z250_RWF.h5 \
+    -o $OUTDIR \
+    $ICTDIR/invisible_cities/config/irene.conf
+}
+
+apt-get update && apt install -y curl build-essential
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y git-lfs
+
+(cd IC && git lfs install && git lfs pull)
+(cd IC_master && git lfs install && git lfs pull)
+
+mkdir master_out
+ICTDIR=`pwd`/IC_master
+OUTDIR=master_out
+(get_output())
+
+mkdir pr_out
+ICTDIR=`pwd`/IC
+OUTDIR=pr_out
+(get_output())
+
 echo "Dummy script standing in for the integration tests to be run on the cluster."
 echo "Given credentials passed in via environment variables, this script should post the job to the majorana cluster and gather the run results."
 echo "It should upload the results somewhere accessible by participants (eg. a gcs bucket)."
