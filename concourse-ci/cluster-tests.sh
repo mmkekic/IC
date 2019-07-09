8ff08be409619f11a19265ec3130bbc120719c00
Miguel Simon Kreuz <s.carton@gmail.com>
2019-07-09 20:40:01 +0200

    testing stuff

--- concourse-ci/cluster-tests.sh
+++ concourse-ci/cluster-tests.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # error out if the credentials haven't been provided
 : "${FAKE_CLUSTER_SSH_PRIVATE_KEY:?}"
 
 get_output () {
-  "${ICTDIR:?}"
-  "${OUTDIR:?}"
+  : "${ICTDIR:?}"
+  : "${OUTDIR:?}"
 
   cd $ICTDIR
   source manage.sh work_in_python_version_no_tests ${IC_PYTHON_VERSION}
 
   city irene \
     -i $ICDIR/database/test_data/electrons_40keV_z250_RWF.h5 \
-    -o $OUTDIR \
+    -o $OUTDIR/irene_result \
     $ICTDIR/invisible_cities/config/irene.conf
 }
 
@@ -24,21 +24,22 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 apt-get install -y git-lfs
 
 (cd IC && git lfs install && git lfs pull)
-(cd IC_master && git lfs install && git lfs pull)
 
 mkdir master_out
 ICTDIR=`pwd`/IC_master
-OUTDIR=master_out
+OUTDIR=`pwd`/master_out
 (get_output)
+ls -lhtr $OUTDIR
 
 mkdir pr_out
 ICTDIR=`pwd`/IC
 OUTDIR=pr_out
 (get_output)
+ls -lhtr $OUTDIR
 
-echo "Dummy script standing in for the integration tests to be run on the cluster."
-echo "Given credentials passed in via environment variables, this script should post the job to the majorana cluster and gather the run results."
-echo "It should upload the results somewhere accessible by participants (eg. a gcs bucket)."
-echo "It should return a nonzero exit status if the results are erroneous."
-echo "----"
-echo "Be careful with handling the credentials unless you want some script kiddie mining bitcoin on your cluster."
+# echo "Dummy script standing in for the integration tests to be run on the cluster."
+# echo "Given credentials passed in via environment variables, this script should post the job to the majorana cluster and gather the run results."
+# echo "It should upload the results somewhere accessible by participants (eg. a gcs bucket)."
+# echo "It should return a nonzero exit status if the results are erroneous."
+# echo "----"
+# echo "Be careful with handling the credentials unless you want some script kiddie mining bitcoin on your cluster."
