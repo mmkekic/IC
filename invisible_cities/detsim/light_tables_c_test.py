646ed5cc9e549bc5520439c4b2cbcc1fa065e38b
mmkekic <mmkekic@gmail.com>
2021-01-25 16:00:47 +0100

    Adapt light tables test for name changes

--- invisible_cities/detsim/light_tables_c_test.py
+++ invisible_cities/detsim/light_tables_c_test.py
@@ -23,13 +23,13 @@ def test_LT_SiPM_optional_arguments(get_dfs):
     fname, psf_df, psf_conf = get_dfs['psf']
     lt = LT_SiPM(fname=fname, sipm_database=datasipm)
     #check the values are read from the table
-    assert lt.el_gap   == psf_conf.loc['EL_GAP'    ].astype(float).value
-    assert lt.active_r == psf_conf.loc['ACTIVE_rad'].astype(float).value
+    assert lt.el_gap_width  == psf_conf.loc['EL_GAP'    ].astype(float).value
+    assert lt.active_radius == psf_conf.loc['ACTIVE_rad'].astype(float).value
     #check optional arguments are set with User Warning
     with warns(UserWarning):
         lt = LT_SiPM(fname=fname, sipm_database=datasipm, el_gap=2, active_r=150)
-        assert lt.el_gap   == 2
-        assert lt.active_r == 150
+        assert lt.el_gap_width  == 2
+        assert lt.active_radius == 150
 
 @given(xs=floats(min_value=-500, max_value=500),
        ys=floats(min_value=-500, max_value=500),
